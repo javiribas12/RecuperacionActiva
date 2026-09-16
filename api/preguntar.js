fe30282742c8ea70a7cac4ed131a83ad1e632dc5
javiribas12 <javiribas12@gmail.com>
2026-09-17 00:40:33 +0200

    Update preguntar.js

--- api/preguntar.js
+++ api/preguntar.js
@@ -67,7 +67,7 @@ Al final de tu respuesta, en una línea aparte, indica qué fuentes de la lista 
 FUENTES_USADAS: fuente 1; fuente 2`;
 }
 
-export default async function handler(req, res) {
+module.exports  async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
