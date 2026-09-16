73c6069a452648f8d2c6bec7d4705ea5c94c009f
javiribas12 <javiribas12@gmail.com>
2026-09-17 00:46:10 +0200

    Update generar-plan.js

--- api/generar-plan.js
+++ api/generar-plan.js
@@ -128,7 +128,7 @@ async function llamarGemini(apiKey, promptTexto) {
   }
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
